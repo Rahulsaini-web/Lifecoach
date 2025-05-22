6cbda22f1c29bf3a8a5de839c7bdcfd6bccb8f72
Rahulsaini-web <Rahulsaini9351@gmail.com>
2025-05-22 14:26:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function Ak() {
+      var demo=document.querySelector('.header-items');
+          demo.classList.toggle('show');
+
+      var king=document.querySelector('.icon-bar i');
+          king.classList.toggle('fa-bars');
+          king.classList.toggle('fa-close');    
+
+    }
